ac5fdfaefdd59881ddaf83fe8beacc8ec22ef18c
frogomelette <98859376+frogomelette@users.noreply.github.com>
2023-06-14 15:22:06 +1000

    UI things

--- Assets/Scripts/ArcadeMenu.cs
+++ Assets/Scripts/ArcadeMenu.cs
@@ -8,10 +8,16 @@ using TMPro;
 
 public class ArcadeMenu : MonoBehaviour
 {
+    public GameObject arrowStart;
+    public GameObject arrowTutorial;
+    public GameObject arrowQuit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        arrowStart.SetActive(false);
+        arrowTutorial.SetActive(false);
+        arrowQuit.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,5 +34,35 @@ public class ArcadeMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void StartArrowEnable()
+    {
+        arrowStart.SetActive(true);
+    }
+
+    public void StartArrowDisable()
+    {
+        arrowStart.SetActive(false);
+    }
+
+    public void TutorialArrowEnable()
+    {
+        arrowTutorial.SetActive(true);
+    }
+
+    public void TutorialArrowDisable()
+    {
+        arrowTutorial.SetActive(false);
+    }
+
+    public void QuitArrowEnable()
+    {
+        arrowQuit.SetActive(true);
+    }
+
+    public void QuitArrowDisable()
+    {
+        arrowQuit.SetActive(false);
     }
 }
