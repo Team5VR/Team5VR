362c8bd4a96797ecff90c4406934f2912da1d53a
hjclark462 <81009898+hjclark462@users.noreply.github.com>
2023-06-16 02:08:14 +1000

    Fixed Transforms in Main Scene Duplicate
    Still need to rescale the player, and scoreboards

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,13 +15,10 @@ public class GameManager : MonoBehaviour
     public BallPodium m_tutorialBall;
     public List<GameObject> m_tutorialPages;
 
+    public UnityAction<XRBaseInteractor> tes;
 
     
     public int CurrentScore = 0;
 
-    void Start()
-    {
-
-    }
-
+    
 }
